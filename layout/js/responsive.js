21e3b0a051a9e5d4d878fd20de5c276996bfc94e
Cristiano Nascimento <cristnascimento@gmail.com>
2020-06-12 19:08:57 -0300

    Removendo diretorios antigos

--- layout/js/responsive.js
+++
@@ -1,9 +0,0 @@
-
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "navigation-bar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navigation-bar";
-  }
-}
