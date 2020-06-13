a97c9d5fdbb77fec10d4db1065e97c5b23cb2067
Cristiano Nascimento <cristnascimento@gmail.com>
2020-06-13 15:25:41 -0300

    Removendo Responsive.js não utilizado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,11 @@ import Header from "./Header";
 import Cadastrar from "./Cadastrar";
 import Visualizar from "./Visualizar";
 import Footer from "./Footer";
-import Responsive from "./Responsive";
 
 import { Container } from "./styles"
 
 import "./css/styles_fonts.css"
+import "./css/styles_responsive.css"
 
 class App extends Component {
    render() {
